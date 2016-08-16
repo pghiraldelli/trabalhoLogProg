43e6ecd8f245b26431a72710812126469c144e74
Carlos Eduardo da Silva Martins <cmartins@mordor.mordor>
2016-08-16 00:47:19 -0300

    resolução parcial da questao 1

--- criancas.hs
+++ criancas.hs
@@ -15,9 +15,28 @@ criancas = ( Pmod [0..7] val acc [7] )
 			(b , 0 , 2 ), (b , 6 , 4 ), (b , 7 , 5 ), (b , 3 , 1 ), (b , 2 , 0 ), (b , 4 , 6 ), (b , 5 , 7 ), (b , 1 , 3 ), 
 			(c , 0 , 1 ), (c , 5 , 4 ), (c , 2 , 3 ), (c , 6 , 7 ), (c , 1 , 0 ), (c , 4 , 5 ), (c , 3 , 2 ), (c , 7 , 6 ) ]
 
-anaSees = reveal[a][q0, q1, r0, r1]
-betoSees = reveal[b][p0, p1, r0, r1]
-carlaSees = reveal[c][p0, p1, q0, q1]
+sussurroPai :: PoAM
+sussurroPai = ( Pmod[0..1] pre acc [0])
+	where 
+		pre = [(0, Neg(Conj[p0, q0, r0])), (1, Conj[p0, q0, r0])]
+		acc = [(a, 0, 0), (a, 1, 1),
+			(b, 0, 0), (b, 0, 1), (b, 1, 0), (b, 1, 1),
+			(c, 0, 0), (c, 0, 1), (c, 1, 0), (c, 1, 1)]
 
-cr = upds criancas [anaSees, betoSees, carlaSees]
+aposSussurro = upd criancas sussurroPai
 
+alguemSabe = Disj[Disj[K a p0, K b q0, K c r0], Disj[K a p1, K b q1, K c r1]]
+
+anuncio :: EpistM -> EpistM
+anuncio modelo = 
+	if isTrue modelo (alguemSabe)
+		then modelo
+	else
+		upd modelo (public (Neg (alguemSabe)))
+
+--anuncio True = m p
+--anuncio False = upd m public (Neg (p))
+
+pergunta1 = anuncio aposSussurro
+pergunta2 = anuncio pergunta1
+pergunta3 = anuncio pergunta2
